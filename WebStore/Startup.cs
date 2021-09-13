5f468c005b0740d2c8d0b5563b8a7153bb54ae11
Lloy94 <Mcdos14@narod.ru>
2021-09-14 03:55:42 +0500

    Добавлен контроллер

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -11,14 +8,13 @@ using Microsoft.Extensions.Hosting;
 namespace WebStore
 {
     public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    {       
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -34,6 +30,9 @@ namespace WebStore
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
+
+                endpoints.MapControllerRoute(
+                    "default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
