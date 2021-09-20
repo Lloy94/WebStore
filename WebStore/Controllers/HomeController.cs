c039682da268c02cfcb2de3175b75ce8ab2a7455
Lloy94 <Mcdos14@narod.ru>
2021-09-20 05:47:26 +0500

    Вынесение работы с сотрудниками в отдельное представление

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -6,27 +6,13 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly List<Employee> __Employees = new()
-        {
-            new Employee { Id = 1, LastName = "Иванов", Name = "Иван", Patronymic = "Иванович", Age = 27, Info = "Первый сотрудник" },
-            new Employee { Id = 2, LastName = "Петров", Name = "Пётр", Patronymic = "Петрович", Age = 31, Info = "Второй сотрудник" },
-            new Employee { Id = 3, LastName = "Сидоров", Name = "Сидор", Patronymic = "Сидорович", Age = 18, Info = "Третий сотрудник" },
-        };
+       
 
-        public Employee __Employee;
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Employees()
-        {
-            return View(__Employees);
-        }
-
-        public IActionResult Employee()
-        {
-            return View(__Employee);
-        }
+      
     }
 }
