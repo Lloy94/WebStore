eca11fa61428f4a115b335da2428dee5e7befa2c
Илья Тимофеев <Mcdos14@ya.ru>
2021-10-01 01:43:13 +0500

    Переименование контроллера Login в Account

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebStore.Controllers
 {
-    public class LoginController : Controller
+    public class AccountController : Controller
     {
         public IActionResult Index()
         {
