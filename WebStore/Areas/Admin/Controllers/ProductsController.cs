590ea0323a2dd2485600f16e93f92741d5763ba4
Илья Тимофеев <Mcdos14@ya.ru>
2021-10-09 12:43:28 +0500

    каталог товаров/заготовки методов для работы с каталогом

--- WebStore/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -1,17 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using WebStore.Services.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductsController : Controller
     {
+        private readonly IProductData _ProductData;
+
+        public ProductsController(IProductData ProductData) => _ProductData = ProductData;
+
         public IActionResult Index()
         {
-            return View();
+            var products = _ProductData.GetProducts();
+            return View(products);
         }
+
+        public IActionResult Edit(int id) => RedirectToAction(nameof(Index));
+
+        public IActionResult Delete(int id) => RedirectToAction(nameof(Index));
     }
 }
