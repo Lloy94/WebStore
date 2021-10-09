5ec6c6c551c98369398a3e722c60f9b0788eeb80
Илья Тимофеев <Mcdos14@ya.ru>
2021-10-09 12:12:01 +0500

    Заготовка области администратора

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -100,8 +100,13 @@ namespace WebStore
                     await context.Response.WriteAsync("Hello World!");
                 });
 
+
                 endpoints.MapControllerRoute(
                     "default", "{controller=Home}/{action=Index}/{id?}");
+              
+                    endpoints.MapControllerRoute(
+                      name: "areas",
+                      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");              
             });
         }
     }
